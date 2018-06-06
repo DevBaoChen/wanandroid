da5c35c770480679fd80fc491c6cf22e14446ce9
baochen <759274701@qq.com>
2018-06-06 23:33:24 +0800

    增加tabs

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 
-import { AboutPage } from '../about/about';
-import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
-
 @Component({
   templateUrl: 'tabs.html'
 })
 export class TabsPage {
 
-  tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
-
+  tab1Root = "IndexPage";
+  tab2Root = "TreePage";
+  tab3Root = "ProjectPage";
+  tab4Root = "AboutPage";
   constructor() {
 
   }
