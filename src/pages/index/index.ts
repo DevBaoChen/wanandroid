14ea07c671c15f1deb909d6a8d5a9e58599d7ed2
baochen <13037890755@163.com>
2018-06-11 16:33:20 +0800

    增加首页滑动监听，和返回顶部功能。

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, ChangeDetectorRef, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, Content } from 'ionic-angular';
 import { HttpProvider } from "../../providers/http/http";
 
 /**
@@ -15,9 +15,12 @@ import { HttpProvider } from "../../providers/http/http";
   templateUrl: 'index.html',
 })
 export class IndexPage {
-  dataList:any=[];
+  @ViewChild(Content) content: Content;
+  dataList: any = [];
+  backTop: any = false;
 
   constructor(
+    private cd: ChangeDetectorRef,
     private httpProvider: HttpProvider,
     public navCtrl: NavController,
     public navParams: NavParams) {
@@ -26,6 +29,25 @@ export class IndexPage {
   ionViewDidLoad() {
     console.log('ionViewDidLoad IndexPage');
     this.getData();
+    this.content.ionScroll.subscribe(($event: any) => {
+
+      try {
+        // console.log($event.scrollTop);//当前滑动的距离
+        if ($event.scrollTop > 300) {
+          this.backTop = true;
+        } else {
+          this.backTop = false;
+        }
+        this.cd.detectChanges();
+      } catch (error) {
+        console.error(error)
+      }
+    })
+  }
+  goTop() {
+    if (this.backTop) {
+      this.content.scrollToTop(500);
+    }
   }
   getData() {
     this.httpProvider.GET("http://www.wanandroid.com/article/list/0/json", "", (res, err) => {
@@ -33,8 +55,8 @@ export class IndexPage {
         console.log(err);
       }
       if (res) {
-        for(var i=0;i< res.data.datas.length;i++){
-          res.data.datas[i].title =res.data.datas[i].title.replace("&mdash;","-").replace("&mdash;","-").replace("&ndash;","-").replace("&ldquo;","“").replace("&rdquo;","”")
+        for (var i = 0; i < res.data.datas.length; i++) {
+          res.data.datas[i].title = res.data.datas[i].title.replace("&mdash;", "-").replace("&mdash;", "-").replace("&ndash;", "-").replace("&ldquo;", "“").replace("&rdquo;", "”")
         }
         this.dataList = res.data.datas;
       }
