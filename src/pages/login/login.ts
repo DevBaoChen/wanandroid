c5b1436cc9248b688d49deafadef67ab4cb66c41
baochen <13037890755@163.com>
2018-06-15 14:44:20 +0800

    增加登录功能.

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { HttpProvider } from "../../providers/http/http";
+import { LoadingProvider } from "../../providers/loading/loading";
 
 /**
  * Generated class for the LoginPage page.
@@ -14,12 +16,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'login.html',
 })
 export class LoginPage {
-
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+   isShowPassword: boolean = false;
+   username:any="";
+   password:any ="";
+  constructor(
+    private loadingProvider: LoadingProvider,
+    private httpProvider: HttpProvider,
+    public navCtrl: NavController, 
+    public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad LoginPage');
+  }
+  showPassword($event) {
+    $event.preventDefault();
+    this.isShowPassword = !this.isShowPassword;
+  }
+  login(){
+    console.log(this.username);
+    console.log(this.password);
+    var params = {
+      username : this.username,
+      password : this.password
+    }
+    this.loadingProvider.presentLoadingDefault();
+    this.httpProvider.POST("http://www.wanandroid.com/user/login",params,(res,err)=>{
+      if (err) {
+        console.log(err);
+      }
+      if (res) {
+        console.log(res);
+        if (!(res.errorCode < 0)) {
+          window.localStorage.setItem("username",res.data.username);
+          window.localStorage.setItem("password",res.data.password);
+          this.navCtrl.pop();
+
+        }else{
+          console.log(res.errorMsg);
+        }
+      }
+    })
+    this.loadingProvider.dismissLoadingDefault();
   }
 
 }
